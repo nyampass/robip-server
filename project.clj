2d21551fdb8d005cf7bb87f03bf1522ac0be3aab
OHTA Shogo <athos0220@gmail.com>
2015-12-24 11:04:43 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,36 @@
+(defproject robip-server "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.stuartsierra/component "0.3.0"]
+                 [compojure "1.4.0"]
+                 [duct "0.5.6"]
+                 [environ "1.0.1"]
+                 [meta-merge "0.1.1"]
+                 [ring "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring-jetty-component "0.3.0"]]
+  :plugins [[lein-environ "1.0.1"]
+            [lein-gen "0.2.2"]]
+  :generators [[duct/generators "0.5.6"]]
+  :duct {:ns-prefix robip-server}
+  :main ^:skip-aot robip-server.main
+  :target-path "target/%s/"
+  :aliases {"gen"   ["generate"]
+            "setup" ["do" ["generate" "locals"]]}
+  :profiles
+  {:dev  [:project/dev  :profiles/dev]
+   :test [:project/test :profiles/test]
+   :uberjar {:aot :all}
+   :profiles/dev  {}
+   :profiles/test {}
+   :project/dev   {:dependencies [[reloaded.repl "0.2.1"]
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [org.clojure/tools.nrepl "0.2.12"]
+                                  [eftest "0.1.0"]
+                                  [kerodon "0.7.0"]]
+                   :source-paths ["dev"]
+                   :repl-options {:init-ns user}
+                   :env {:port 3000}}
+   :project/test  {}})
